22c376d8960624656ca6857195796638712b8045
yasuingh <manjusingh7204@gmail.com>
2026-06-21 01:03:53 +0530

    Force production settings in build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+DJANGO_SETTINGS_MODULE=config.settings.production python manage.py collectstatic --noinput
+DJANGO_SETTINGS_MODULE=config.settings.production python manage.py migrate
