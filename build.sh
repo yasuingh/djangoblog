55ae3645cfe22799d5ba9919044add37e1ab96fb
yasuingh <manjusingh7204@gmail.com>
2026-06-21 01:12:59 +0530

    Debug DATABASE_URL

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
+echo "DATABASE_URL is: $DATABASE_URL"
 DJANGO_SETTINGS_MODULE=config.settings.production python manage.py collectstatic --noinput
 DJANGO_SETTINGS_MODULE=config.settings.production python manage.py migrate
